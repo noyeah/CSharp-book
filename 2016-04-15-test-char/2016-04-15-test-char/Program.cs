5e18c8b469cf200dbdfa22fb055ceccb57c8dd60
noyeah <dlguswp23@gmail.com>
2016-04-16 00:04:00 +0900

    Add 2016-04-16 2016-04-05-test-char \uxxxx

---
+++ 2016-04-15-test-char/2016-04-15-test-char/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2016_04_15_test_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = '\u2023';
+            Console.WriteLine(ch);
+            Console.ReadKey();
+        }
+    }
+}
