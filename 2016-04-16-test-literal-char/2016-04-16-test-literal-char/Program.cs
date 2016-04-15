c6133296ebe2defe7c52c654efd40f2ab5cd8654
noyeah <dlguswp23@gmail.com>
2016-04-16 01:17:04 +0900

    Add 2016-04-16-test-literal-char beta

---
+++ 2016-04-16-test-literal-char/2016-04-16-test-literal-char/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2016_04_16_test_literal_char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1냠\'냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("2냠\"냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("3냠\0냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("4냠\a냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("5냠\b냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("6냠\f냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("7냠\n냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("8냠\r냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("9냠\t냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("10냠\u2023냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("11냠\x1111냠냠"); Console.WriteLine("----------------");
+            Console.WriteLine("12냠\v냠냠"); Console.WriteLine("----------------");
+            Console.ReadKey();
+
+        }
+    }
+}
