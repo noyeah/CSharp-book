5b4ca5bf2a9b9d4d3e4a090227ff7da53c7ce705
noyeah <dlguswp23@gmail.com>
2016-04-17 05:12:30 +0900

    Add 2016-04-17-Euler-1

---
+++ 2016-04-17-Euler-1/2016-04-17-Euler-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2016_04_17_Euler_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, sum=0;
+            while (++num < 1000)
+            {
+                if (num % 3 == 0 || num % 5 == 0)
+                {
+                    sum += num;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+    }
+}
