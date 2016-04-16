4144a2e22ca77af540a2351ef4750b6bb2ae2a7c
noyeah <dlguswp23@gmail.com>
2016-04-17 03:58:46 +0900

    Add 2016-04-17-test-for-break

---
+++ 2016-04-17-test-for-break/2016-04-17-test-for-break/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _2016_04_17_test_for_break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int i = 2;
+
+                while (true)
+                {
+                    int j = 1;
+
+                    while (true)
+                    {
+                        Console.WriteLine(i + " * " + j + " = " + (i * j));
+
+                        if (++j > 9) // ++j is biger than 9 is true
+                        {
+                            break;
+                        }
+                    }
+
+                    if (++i > 9)
+                    {
+                        break;
+                    }
+                }
+
+            int a = 9;
+            string txt = (++a > 9)? "true" : "false";
+            Console.WriteLine(txt);
+            Console.ReadKey();
+        }
+    }
+}
