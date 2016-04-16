41205b0f6117897226dc78ecfbabf57a03966c42
noyeah <dlguswp23@gmail.com>
2016-04-17 02:48:27 +0900

    Add 2016-04-17-test-ternary_operator why ternary_operator ? because operand is 3! hahhaha!

---
+++ 2016-04-17-test-ternary_operator/2016-04-17-test-ternary_operator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2016_04_17_test_ternary_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            string result = (num % 2 == 0) ? "짝수" : "홀수";
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
