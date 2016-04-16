5469d22d42dd18c391ae646569d8452631a86979
noyeah <dlguswp23@gmail.com>
2016-04-17 04:24:57 +0900

    Add 2016-04-17-test-while-continue

---
+++ 2016-04-17-test-while-continue/2016-04-17-test-while-continue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2016_04_17_test_while_continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0,num =0;
+            while(true)
+            {
+                num++;
+                if (num == 100) // if num to be 100 continue -> sum +=101 and while end
+                {
+                    break;
+                }
+                if (num % 2 == 0)
+                {
+                    continue; 
+                }
+                sum += num;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
