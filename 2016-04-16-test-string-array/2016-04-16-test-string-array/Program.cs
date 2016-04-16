114d42cf1244846c4c4bfc3c08d1767c989e614d
noyeah <dlguswp23@gmail.com>
2016-04-16 22:53:59 +0900

    Add 2016-04-16-test-string-array

---
+++ 2016-04-16-test-string-array/2016-04-16-test-string-array/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2016_04_16_test_string_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "Hello World!";
+            char ch1 = txt[0];
+            char ch2 = txt[3];
+            Console.WriteLine(ch1);
+            Console.WriteLine(ch2);
+            Console.ReadKey();
+        }
+    }
+}
