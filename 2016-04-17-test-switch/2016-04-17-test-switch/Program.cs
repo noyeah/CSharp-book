39c33f96ad93a3f44ee52d2620bc9e78533554d7
noyeah <dlguswp23@gmail.com>
2016-04-17 02:58:39 +0900

    Add 2016-04-17-test-switch

---
+++ 2016-04-17-test-switch/2016-04-17-test-switch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2016_04_17_test_switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = (char)Console.Read();
+            switch (ch)
+            {
+                case '9': // If you press the 9 output is case '1' wow!
+                case '1':
+                    Console.WriteLine("one");
+                    break;
+                default:
+                    Console.WriteLine("cwtt rolll");
+                    break;
+             }
+            Console.ReadKey();
+        }
+    }
+}
