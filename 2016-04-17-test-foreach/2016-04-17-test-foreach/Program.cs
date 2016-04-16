5957f2f3fbc33a20f0fd5689fe8746fd63c40a5f
noyeah <dlguswp23@gmail.com>
2016-04-17 03:29:31 +0900

    Add 2016-04-17-test-foreach

---
+++ 2016-04-17-test-foreach/2016-04-17-test-foreach/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2016_04_17_test_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+
+            foreach(int num in arr)
+            {
+                Console.WriteLine(num); // Foreach is arr array[0-end] in num and cwtt 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
