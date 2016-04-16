c63caf47d8fcb70c442ee9b689c9444cc7fbcadd
noyeah <dlguswp23@gmail.com>
2016-04-16 23:27:38 +0900

    Add 2016-04-16-test-array-change

---
+++ 2016-04-16-test-array-change/2016-04-16-test-array-change/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2016_04_16_test_array_change
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[60];
+            num[0] = 10;
+            Console.WriteLine(num[0]);
+            num = new int[120];
+            Console.WriteLine(num[0]);
+            Console.ReadKey();
+        }
+    }
+}
