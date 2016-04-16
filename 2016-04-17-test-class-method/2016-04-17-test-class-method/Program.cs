92d0574fb7473af383b2f1f018cea729ea6304f0
noyeah <dlguswp23@gmail.com>
2016-04-17 06:19:33 +0900

    Add 2016-04-17-test-class-method

---
+++ 2016-04-17-test-class-method/2016-04-17-test-class-method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2016_04_17_test_class_method
+{
+    class Mathmatics
+    {
+        public int GetAreaOfSquare(int x)
+        {
+            return x * x;
+        }
+
+        public int GetValue()
+        {
+            return 10;
+        }
+
+        public void Output(string prefix, int value)
+        {
+            Console.WriteLine(prefix + value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathmatics m = new Mathmatics();
+            int result = m.GetAreaOfSquare(m.GetValue());
+
+            m.Output("결과 : ", result);
+            Console.ReadKey();
+        }
+    }
+}
