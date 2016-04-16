0f987b61fc4937a470c64a7d83272e4fc7b2cb7b
noyeah <dlguswp23@gmail.com>
2016-04-17 03:21:44 +0900

    Add 2016-04-17-test-for_ninetable

---
+++ 2016-04-17-test-for_ninetable/2016-04-17-test-for_ninetable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2016_04_17_test_for_ninetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine(i + " * " + j + " = " + (i*j));
+                }
+                Console.WriteLine("-------------------------------------");
+            }
+            Console.ReadKey();
+        }
+    }
+}
