28b98c7e43795d57e09b6f4bec78a8b4009bdf8b
unknown <>
2016-04-22 01:22:56 +0900

    Add 2016-04-22-test-class-field

---
+++ 2016-04-17-test-class-field/2016-04-17-test-class-field/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _2016_04_17_test_class_field
+{
+    class book
+    {
+        public string Title;
+        public decimal ISBN13;
+        public string Cointents;
+        public string Author;
+        public int PageCount;
+
+        public void Open()
+        {
+            Console.WriteLine("Book id opened");
+        }
+
+        public void Close()
+        {
+            Console.WriteLine("Book is closed");
+        }
+
+        public void WriteIF(bool output, string txt)
+        {
+            if (output == false)
+            {
+                return; // 값의 반환 말고도 점프 구문의 역할도 있다.
+            }
+
+            Console.WriteLine(txt);
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            book b = new book();
+            b.WriteIF(false, "kieek");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2016-04-22-test-class-field/2016-04-22-test-class-field/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2016_04_22_test_class_field
+{
+    class Book
+    {
+        public int id;
+        public char ch = '5';
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.id = 1;
+            char ch1 = book.ch;
+            Console.WriteLine(book.id);
+            Console.WriteLine(ch1);
+            Console.ReadKey();
+            
+        }
+    }
+}
