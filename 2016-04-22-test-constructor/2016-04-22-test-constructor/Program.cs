d0916381f3a7589eeadfd06e5d5183815703d60b
unknown <>
2016-04-22 23:00:26 +0900

    Add 2016-04-22-test-constructor

---
+++ 2016-04-22-test-constructor/2016-04-22-test-constructor/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _2016_04_22_test_constructor
+{
+    class Book
+    {
+        public string Title;
+        public decimal ISBN13;
+        public string Author;
+        public Book()
+        {
+            Console.WriteLine("생성자 생김 뿌ㅡ우");
+        }
+
+        public Book(string title)
+        {
+            Title = title;
+        }
+
+        public Book(string title, decimal isbn13)
+        {
+            Title = title;
+            ISBN13 = isbn13;
+        }
+
+        public Book(string title, decimal isbn13, string author)
+        {
+            Title = title;
+            ISBN13 = isbn13;
+            Author = author;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(Title);
+            Console.WriteLine(ISBN13);
+            Console.WriteLine(Author);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Book a = new Book("a");
+            a.Output();
+            Console.WriteLine("-----------------");
+            Book b = new Book("b",1111);
+            b.Output();
+            Console.WriteLine("-----------------");
+            Book c = new Book("c",2222,"wwww");
+            c.Output();
+            Console.WriteLine("-----------------");
+            Console.ReadKey();
+
+        }
+    }
+}
