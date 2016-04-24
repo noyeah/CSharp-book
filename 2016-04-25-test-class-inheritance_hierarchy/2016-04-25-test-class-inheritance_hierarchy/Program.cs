207608d35454872be3eb430fefb830d8e2c4c9f4
noyeah <dlguswp23@gmail.com>
2016-04-25 05:53:08 +0900

    Add 2016-04-25-test-class-inheritance_hierarchy

---
+++ 2016-04-25-test-class-inheritance_hierarchy/2016-04-25-test-class-inheritance_hierarchy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _2016_04_25_test_class_inheritance_hierarchy
+{
+    class grandFather
+    {
+        public int gage;
+
+        public void grandage()
+        {
+            Console.WriteLine(gage);
+        }
+
+    }
+
+    class father : grandFather
+    {
+        public int fage;
+
+        public void fatherage()
+        {
+            Console.WriteLine(fage);
+        }
+
+    }
+
+    class me : father
+    {
+        public int age;
+        
+        public void meage()
+        {
+            Console.WriteLine(age);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            me m = new me();
+            m.age = 21;
+            m.meage();
+            m.fage = m.age + 30;
+            m.fatherage();
+            m.gage = m.fage + 30;
+            m.grandage();
+
+            Console.ReadKey();
+        }
+    }
+}
