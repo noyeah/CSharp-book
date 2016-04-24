4c34aca60745be80d9f1cf5ae99d7dbbe466092b
noyeah <dlguswp23@gmail.com>
2016-04-25 05:51:43 +0900

    Add 2016-04-25-test-namespace

---
+++ 2016-04-24-test-namespace/2016-04-24-test-namespace/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        public void p()
+        {
+            Console.WriteLine("test 네임 스페이스 p() 함수 실행");
+        }
+
+    }
+
+}
+
+namespace _2016_04_24_test_namespace
+{
+    class Program
+    {
+        public void p()
+        {
+            Console.WriteLine("_2016_04_24_test_namespace 네임 스페이스 p() 함수 실행");
+
+        }
+        static void Main(string[] args)
+        {
+            test.Program A = new test.Program();
+            A.p();
+            _2016_04_24_test_namespace.Program B = new _2016_04_24_test_namespace.Program();
+            B.p();
+            Console.ReadKey();
+
+        }
+    }
+}
