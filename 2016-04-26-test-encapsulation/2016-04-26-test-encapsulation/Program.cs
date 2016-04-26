92b5b92cb742d7a810755e7e50eba646d6b0f186
noyeah <dlguswp23@gmail.com>
2016-04-26 10:29:56 +0900

    Add 2016-04-26-test-encapsulation

---
+++ 2016-04-26-test-encapsulation/2016-04-26-test-encapsulation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2016_04_26_test_encapsulation
+{
+    class Circle
+    {
+        private double Pi = 3.14;
+
+        private double GetArea(double radius)
+        {
+            return radius * radius * Pi;
+        }
+
+        public void Print(double value)
+        {
+            Console.WriteLine(GetArea(value));
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle one = new Circle();
+
+            one.Print(10);
+            Console.ReadKey();
+        }
+    }
+}
