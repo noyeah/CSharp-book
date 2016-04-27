9775fa4a2e6004cedf27d11b26af36414727bc1b
noyeah <dlguswp23@gmail.com>
2016-04-28 07:33:53 +0900

    Add 2016-04-28-test-foreach-f

---
+++ 2016-04-27-test-foreach-f/2016-04-27-test-foreach-f/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2016_04_27_test_foreach_f
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] A = new int[] { 80, 90, 90, 50, 100 };
+
+            foreach(int num in A)
+            {
+                sum += num;
+            }
+            // sum = A학생의 성적의 합
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
